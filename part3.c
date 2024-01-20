e41a31639155b6ee48590fd6bd3ad1bd401028ec
sailings <sailings@126.com>
2024-01-20 21:37:30 +0800

    update

--- part3.c
+++ part3.c
@@ -106,6 +106,32 @@ void free_table(Table *table) {
     free(table);
 }
 
+InputBuffer *new_input_buffer() {
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+
+void print_prompt() { printf("db > "); }
+
+void read_input(InputBuffer *input_buffer) {
+    ssize_t bytes_read =
+            getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Ignore trailing newline
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
 void close_input_buffer(InputBuffer *input_buffer) {
     free(input_buffer->buffer);
     free(input_buffer);
@@ -127,7 +153,8 @@ PrepareResult prepare_statement(InputBuffer *input_buffer,
         statement->type = STATEMENT_INSERT;
         int args_assigned = sscanf(
                 input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-                statement->row_to_insert.username, statement->row_to_insert.email);
+                statement->row_to_insert.username, statement->row_to_insert.email
+        );
         if (args_assigned < 3) {
             return PREPARE_SYNTAX_ERROR;
         }
@@ -172,31 +199,6 @@ ExecuteResult execute_statement(Statement *statement, Table *table) {
     }
 }
 
-InputBuffer *new_input_buffer() {
-    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
-    input_buffer->buffer = NULL;
-    input_buffer->buffer_length = 0;
-    input_buffer->input_length = 0;
-
-    return input_buffer;
-}
-
-void print_prompt() { printf("db > "); }
-
-void read_input(InputBuffer *input_buffer) {
-    ssize_t bytes_read =
-            getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-
-    if (bytes_read <= 0) {
-        printf("Error reading input\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Ignore trailing newline
-    input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0;
-}
-
 int main(int argc, char *argv[]) {
     Table *table = new_table();
     InputBuffer *input_buffer = new_input_buffer();
@@ -204,15 +206,14 @@ int main(int argc, char *argv[]) {
         print_prompt();
         read_input(input_buffer);
 
+
         if (input_buffer->buffer[0] == '.') {
-            if (input_buffer->buffer[0] == '.') {
-                switch (do_meta_command(input_buffer, table)) {
-                    case (META_COMMAND_SUCCESS):
-                        continue;
-                    case (META_COMMAND_UNRECOGNIZED_COMMAND):
-                        printf("Unrecognized command '%s'\n", input_buffer->buffer);
-                        continue;
-                }
+            switch (do_meta_command(input_buffer, table)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
             }
         }
 
